b55ffc2b9f40661d635a18c992d2de02ed5a3d30
Revanth Kolla <revanthkolla23@gmail.com>
2025-07-05 23:02:49 +0530

    Contact Form Handling

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,38 @@
 import express from "express"
 import cors from "cors"
+import nodemailer from "nodemailer"
+import dotenv from "dotenv"
 
+dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors());
+app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res) => {
-  res.send("Hello World");
+const transporter= nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.EMAIL_PASS
+  }
 })
 
-app.post('/api/contact', (req, res) => {
+app.post('/api/contact', async (req, res) => {
 
   try {
     const {name, email, message} = req.body;
 
-    res.status(200).json({ success: true, message: "Message sent successfully" });
+    await transporter.sendMail({
+      from: process.env.SENDER_EMAIL,
+      to: process.env.RECIEVER_EMAIL,
+      subject: "Message from Portfolio's Contact Form",
+      text: `Email : ${email}
+             Name : ${name}
+             Message: ${message}`
+    })
 
-    console.log(name, email, message);
+    res.status(200).json({ success: true, message: "Message sent successfully" });
 
   } catch (error) {
     res.status(400).json({success: false,message: "Failed to send the message"});
